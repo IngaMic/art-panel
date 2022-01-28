7dddbb5e55bf7bdb99908e2b1b12bc65729a1f88
Inga Michneviciute <ingeramich@gmail.com>
2022-01-28 13:36:51 +0100

    searchbar

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { SearchDrawer } from "../components/shared/SearchDrawer";
 export const Navbar = () => {
     const [active, setActive] = useState(false);
     const handleClick = () => {
@@ -23,56 +24,48 @@ export const Navbar = () => {
                             strokeLinejoin="round"
                             strokeWidth={2}
                             d="M4 6h16M4 12h16M4 18h16"
-                        />{" "}
-                             
+                        />
                     </svg>
-                       
                 </button>
                    
                 {/* a ternary operator to decide whether or not to display the content of the div  */}
-                   
                 <div
                     className={`${
                         active ? "" : "hidden"
-                    }   w-full lg:inline-flex lg:flex-grow lg:w-auto md:bg-white text-white md:text-gray-900 py-5 border-b `}
+                    } w-full lg:inline-flex lg:flex-grow lg:w-auto md:bg-white text-gray-900 py-5`}
                 >
-                    <div className="flex flex-col lg:inline-flex lg:flex-row flex-end w-full lg:w-auto items-start lg:items-center lg:h-auto justify-end">
+                    <div className="flex flex-col w-full lg:w-auto items-start lg:items-center lg:h-auto justify-end">
                         <a href="/">
                             <div className="fixed ml-6 md:ml-16 top-0 w-32 bg-black">
-                                <div className="top-0 h-20 w-32 bg-black text-white">
-                                    Soup
-                                </div>
+                                <div className="top-0 h-20 w-32 bg-black text-white"></div>
                                 <div className="border-b w-100"></div>
-                                <div className="h-10 w-32 bg-black border-t border-wh">
-                                    Soup Two
+                                <div className="h-10 w-32 bg-black border-t border-wh text-center text-white">
+                                    depiction
                                 </div>
                             </div>
                         </a>
-                               
+                    </div>
+                    <div className="flex flex-col lg:flex-row-reverse">
                         <a
                             href="/"
-                            className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded items-center active:font-bold hover:font-bold "
+                            className=" w-full lg:w-auto px-3 py-2 rounded items-center active:font-bold hover:font-bold"
                         >
-                                            Link       
+                            Link
                         </a>
-                                         
                         <a
                             href="/"
-                            className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded items-center jactive:font-bold hover:font-bold"
+                            className="w-full lg:w-auto px-3 py-2 rounded items-center active:font-bold hover:font-bold"
                         >
-                                            Link        
+                            Link
                         </a>
-                               
                         <a
                             href="/"
-                            className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded items-center active:font-bold hover:font-bold"
+                            className="w-full lg:w-auto px-3 py-2 rounded items-center active:font-bold hover:font-bold"
                         >
-                                            Link       
+                            Link
                         </a>
                     </div>
-                       
                 </div>
-                 
             </nav>
         </>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import Featured from "../components/blocks/Featured";
 import FlexWrap from "../components/shared/FlexWrap";
+import { SearchDrawer } from "../components/shared/SearchDrawer";
 
 function Home() {
     return (
         <div className="m-8 md:m-16">
+            <div className="flex justify-end mb-5 -mt-10">
+                <SearchDrawer />
+            </div>
             <div className="flex flex-col xl:flex-row">
                 <FlexWrap width="w-full" />
                 <Featured />
